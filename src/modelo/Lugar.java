047121188b7bed07a2aa4056db049a0b581e7310
BrayanMQ <brayanmq11288@estudiantec.cr>
2019-11-25 20:32:01 -0600

    Getters setters Lugar

--- src/modelo/Lugar.java
+++ src/modelo/Lugar.java
@@ -23,6 +23,64 @@ public abstract class Lugar implements IConstants{
         this.listaEstudiantes = new ArrayList<>(); 
         this.cuposDisponibles = CUPOS_DISPONIBLES;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public ArrayList<Estudiante> getListaEstudiantes() {
+        return listaEstudiantes;
+    }
+
+    public void setListaEstudiantes(ArrayList<Estudiante> listaEstudiantes) {
+        this.listaEstudiantes = listaEstudiantes;
+    }
+
+    public int getCuposDisponibles() {
+        return cuposDisponibles;
+    }
+
+    public void setCuposDisponibles(int cuposDisponibles) {
+        this.cuposDisponibles = cuposDisponibles;
+    }
+    
+    
     
     @Override
     public boolean equals(Object obj) {
